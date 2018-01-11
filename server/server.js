d2bb54f34c127ca11105ab968c4673d0ee5c1cc8
FoxChrisRealTheGit <cfox1290@gmail.com>
2018-01-11 11:02:02 -0700

    test works

--- server/server.js
+++ server/server.js
@@ -6,28 +6,44 @@ const express = require('express'),
     passport = require('passport'),
     Auth0Strategy = require('passport-auth0');
 
-    const app = express();
-    app.use(bodyparser.json());
+const app = express();
+app.use(bodyparser.json());
 
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-        passport.use(new Auth0Strategy({
-            domain: process.env.AUTH_DOMAIN,
-            clientID: process.env.AUTH_CLIENTID,
-            clientSecret: process.env.AUTH_CLIENTSECRET,
-            callbackURL: process.env.AUTH_CALLBACK_URL,
-            scope:"openid profile"
-        }))
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENTID,
+    clientSecret: process.env.AUTH_CLIENTSECRET,
+    callbackURL: process.env.AUTH_CALLBACK_URL,
+    scope: "openid profile"
+}, function (accessToken, refreshToken, extraParams, profile, done) {
+    console.log(profile);
+    done(null, profile);
+}))
+
+
+passport.serializeUser((profile, done)=>{
+    done(null, profile);
+})
+passport.deserializeUser((profile, done)=>{
+    done(null, profile);
+})
+
+
+app.get('/auth', passport.authenticate("auth0"))
+app.get('/auth/callback', passport.authenticate("auth0", {
+    successRedirect: "http://localhost:3000/",
+    failureRedirect: "/auth"
+}))
 
 
 
-
-
-    const {SERVER_PORT} = process.env
-    app.listen(SERVER_PORT, function(){console.log(`listening on port ${SERVER_PORT}`)});
+const { SERVER_PORT } = process.env
+app.listen(SERVER_PORT, function () { console.log(`listening on port ${SERVER_PORT}`) });
